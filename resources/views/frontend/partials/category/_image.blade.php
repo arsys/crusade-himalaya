a72605ed88cc149f5dc2d0234c9e2219b53a9aa5
Kajol2608 <rauniyarkajol2608@gmail.com>
2018-12-25 12:06:06 +0545

    category page

--- resources/views/frontend/partials/category/_image.blade.php
+++ resources/views/frontend/partials/category/_image.blade.php
@@ -1,6 +1,15 @@
-<section class="uk-container-expand">
-  	<div class="uk-height-large uk-background-cover uk-light uk-flex" uk-parallax="bgy: -200, media: @m" style="background-image: url('https://source.unsplash.com/1200x800/?arts'); ">
-		<p class="uk-width-1-2@m uk-text-center uk-margin-auto uk-margin-auto-vertical">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque nisi consectetur vel, doloremque molestiae porro reiciendis dolor natus autem, repellendus?</p>
-	
+<section class="uk-container-expand category" uk-grid>
+	<div class="uk-height-large  uk-width-*@s  uk-background-cover uk-light category-cover" data-src="https://source.unsplash.com/1200x800/?bird" uk-img>
+		<h1 class="page-heading uk-margin-large-left">Category</h1>
+		<h3 class="page-subheading uk-margin-large-left uk-margin-top uk-margin-bottom">Lorem ipsum dolor sit amet.</h3>
+		<div class="cat-breadcrumb ">
+			<ul class="uk-breadcrumb" id="cat-breadcrumb">
+				<li><a href="#">list</a></li>
+				<li><a href="#">list</a></li>
+				<li class="uk-disabled"><a>Disabled</a></li>
+				<li><span>Active</span></li>
+			</ul>
+		</div>
 	</div>
 </section>
+
--- resources/views/frontend/partials/category/_package.blade.php
+++ resources/views/frontend/partials/category/_package.blade.php
@@ -1,53 +1,39 @@
 <div class="uk-section uk-section-small uk-section-default">
     <div class="uk-container">
-    	 <h2 class="heading-primary uk-text-center "><span>About Us</span></h2>
+    	 <h2 class="heading-primary uk-text-center "><span>Category </span></h2>
         <div class="text-align-left uk-margin-medium-top" uk-grid>
-            <div class="uk-width-1-4@m" >
-        	   <div>
-        			<h4>Divider</h4>
-        			<ul class="uk-list uk-list-large uk-list-divider">
-            			<li>List item 1</li>
-            			<li>List item 2</li>
-            			<li>List item 3</li>
-        			</ul>
-    			</div>
-            </div>
-       
-        	<div class="uk-width-3-4@m" >
-        		<div class="uk-child-width-1-3@m" uk-grid>
-				   @for($a=1; $a<=6; $a++)
-			    	<div class="package-info">
-				        <div class="uk-card uk-card-default">
-				            <div class="uk-card-media-top">
-				                <a href="/product"><img src="{{asset('img/Tours/annapurna.jpg')}}" alt="Image"></a>
-				            </div>
+    		<div class="uk-child-width-1-3@m" uk-grid>
+			   @for($a=1; $a<=6; $a++)
+		    	<div class="package-info">
+			        <div class="uk-card uk-card-default">
+			            <div class="uk-card-media-top">
+			                <a href="/product"><img src="{{asset('img/Tours/annapurna.jpg')}}" alt="Image"></a>
+			            </div>
 
-			               	<div class="uk-card-body">
-				            	<div class="uk-card-title"><a href="#"><h4>Package Title</h4></a>
-				            	</div>
-				                
-				              	<div class="uk-flex uk-flex-center">
-									<div class="uk-flex uk-flex-column package-details">
-										<span class="price-info">15</span>
-										<span class="price-info">Days</span>
-									</div>
-									<div class="uk-flex uk-flex-column package-details">
-										<span class="price-info">From</span>
-										<span class="price-info">USD</span>
-									</div>
-									<div class="uk-flex uk-flex-row package-details">
-										
-										<span class="price"><sup>$</sup>1500</span>
-									</div>
-								
+		               	<div class="uk-card-body">
+			            	<div class="uk-card-title"><a href="#"><h4>Package Title</h4></a>
+			            	</div>
+			                
+			              	<div class="uk-flex uk-flex-center">
+								<div class="uk-flex uk-flex-column package-details">
+									<span class="price-info">15</span>
+									<span class="price-info">Days</span>
 								</div>
-				            </div>
-				        </div>
-				    </div>
-				    @endfor
-				</div>
-
-        	</div>
+								<div class="uk-flex uk-flex-column package-details">
+									<span class="price-info">From</span>
+									<span class="price-info">USD</span>
+								</div>
+								<div class="uk-flex uk-flex-row package-details">
+									
+									<span class="price"><sup>$</sup>1500</span>
+								</div>
+							
+							</div>
+			            </div>
+			        </div>
+			    </div>
+			    @endfor
+			</div>
         </div>
 
     </div>

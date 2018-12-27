f7b4736325fd43366e4009968d3ebb2f62c22126
Bantawa Pawan <rahunn3@gmail.com>
2018-12-27 13:35:31 +0545

    odd even destination in frontend

--- resources/views/frontend/partials/home/_destination.blade.php
+++ resources/views/frontend/partials/home/_destination.blade.php
@@ -4,31 +4,31 @@
             <h2 class=" uk-text-center home-heading-primary uk-text-center uk-margin-large-bottom"><span>DESTINATION</span></h2>
             <div class="uk-grid-collapse uk-child-width-1-2 uk-child-height-1-2 uk-grid" uk-grid="">
                 <div class="uk-first-column">
+                    @foreach($odds as $odd)
+                    @if($loop->iteration % 2 == 0)
                     <div class="destination">
-                        <a href="/destination"><img src="https://source.unsplash.com/1200x500/?annapurna" alt="" class="uk-padding-xsmall dest-image"></a>
+                        <a href="/destination"><img src="{{ asset($odd->thumb) }}" alt="" class="uk-padding-xsmall dest-image"></a>
                         <div class="dest-topic">
                             <a href="/destination">
-                                <div class="dest-title">nepal</div>
+                                <div class="dest-title">{{ $odd->title }}</div>
                             </a>
                         </div>
                     </div>
+                    @endif
+                    @endforeach
                     <div class="uk-child-width-expand@s uk-text-center uk-grid uk-grid-collapse" uk-grid="">
-                        <div class="uk-first-column uk-panel uk-padding-xsmall destination">
-                            <a href="/destination"><img src="https://source.unsplash.com/1200x500/?heli" alt="" class="dest-image"></a>
+                        @foreach($odds as $odd)
+                        @if($loop->iteration % 2 != 0)
+                        <div class="{{$loop->first ? 'uk-first-column': '' }} uk-panel uk-padding-xsmall destination">
+                            <a href="/destination"><img src="{{ asset($odd->thumb) }}" alt="" class="dest-image"></a>
                             <div class="dest-topic">
                                 <a href="/destination">
-                                    <div class="dest-title">Annapurna Base Camp</div>
+                                    <div class="dest-title">{{ $odd->name }}</div>
                                 </a>
                             </div>
                         </div>
-                        <div class="uk-panel uk-padding-xsmall destination">
-                            <img src="https://source.unsplash.com/1200x500/?nepal" alt="" class="dest-image">
-                            <div class="dest-topic">
-                                <a href="/destination">
-                                    <div class="dest-title">Heli Base Camp</div>
-                                </a>
-                            </div>
-                        </div>
+                        @endif
+                        @endforeach
                     </div>
                 </div>
 
@@ -37,7 +37,7 @@
                     <div class="uk-child-width-expand@s uk-text-center uk-grid uk-grid-collapse" uk-grid="">
                         @foreach($evens as $even)
                         @if($loop->iteration % 2 != 0)
-                        <div class="uk-first-column uk-panel uk-padding-xsmall destination">
+                        <div class="{{ $loop->first ? 'uk-first-column' : '' }} uk-panel uk-padding-xsmall destination">
                             <a href="/destination"><img src="{{ asset($even->thumb) }}" alt="" class="dest-image"></a>
                             <div class="dest-topic">
                                 <a href="/destination">

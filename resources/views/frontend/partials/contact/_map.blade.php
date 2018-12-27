c223d2a6f7e946af521c1c6a72f6f78b8b099e8e
Bantawa Pawan <rahunn3@gmail.com>
2018-12-27 17:01:15 +0545

    google maps

--- resources/views/frontend/partials/contact/_map.blade.php
+++ resources/views/frontend/partials/contact/_map.blade.php
@@ -1,3 +1,21 @@
 	<div class="map-wrapper uk-section-default">
-	    <img src="https://source.unsplash.com/1000x400/?map" alt="" style="width: 100%;">
+	    <div id="map"></div>
 	</div>
+@section('scripts')
+<script>
+    function initMap() {
+        var uluru = {lat: 27.717482, lng: 85.310671};
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 18,
+            center: uluru
+        });
+        var marker = new google.maps.Marker({
+            position: uluru,
+            map: map
+        });
+    }
+</script>
+<script async defer
+src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCL_fHyHReJANrNN1TACrswYpo_dSCAjkI&callback=initMap">
+</script>
+@stop

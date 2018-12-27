1327e8acc5a28c60c19f3159affc3c29e74d6fd4
Bantawa Pawan <rahunn3@gmail.com>
2018-12-27 17:10:42 +0545

    map

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -10,3 +10,21 @@
 @include ('frontend.partials.contact._map')
 <!--   map end -->
 @stop
+@section('scripts')
+<script>
+    function initMap() {
+        var uluru = {lat: 27.717482, lng: 85.310671};
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 18,
+            center: uluru
+        });
+        var marker = new google.maps.Marker({
+            position: uluru,
+            map: map
+        });
+    }
+</script>
+<script async defer
+src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCL_fHyHReJANrNN1TACrswYpo_dSCAjkI&callback=initMap">
+</script>
+@stop
--- resources/views/frontend/partials/contact/_map.blade.php
+++ resources/views/frontend/partials/contact/_map.blade.php
@@ -1,21 +1,3 @@
 	<div class="map-wrapper uk-section-default">
 	    <div id="map" style="height: 300px;"></div>
 	</div>
-@section('scripts')
-<script>
-    function initMap() {
-        var uluru = {lat: 27.717482, lng: 85.310671};
-        var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 18,
-            center: uluru
-        });
-        var marker = new google.maps.Marker({
-            position: uluru,
-            map: map
-        });
-    }
-</script>
-<script async defer
-src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCL_fHyHReJANrNN1TACrswYpo_dSCAjkI&callback=initMap">
-</script>
-@stop

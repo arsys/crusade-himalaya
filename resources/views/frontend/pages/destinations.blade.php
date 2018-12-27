e4fa34e39e2ce0c1dfaa62014b1481c12cccaa79
Bantawa Pawan <rahunn3@gmail.com>
2018-12-27 17:41:40 +0545

    long & lat for map

--- resources/views/frontend/pages/destinations.blade.php
+++ resources/views/frontend/pages/destinations.blade.php
@@ -4,7 +4,7 @@
 <section class="uk-container-expand about" uk-grid>
 	<div class="uk-height-large  uk-width-*@s  uk-background-cover uk-light page-cover uk-background-fixed" data-src="{{ asset($region->path) }}" uk-img>
 		<h1 class="page-heading uk-margin-large-left">{{ $region->name }}</h1>
-		<h3 class="page-subheading uk-margin-large-left uk-margin-top uk-margin-bottom">{{ $region->description }}</h3>
+		<p class="page-subheading uk-margin-large-left uk-margin-top uk-margin-bottom">{{ $region->description }}</p>
 		<div class="breadcrumb uk-visible@l">
 			<ul class="uk-breadcrumb" id="breadcrumb">
 				<li><a href="/" uk-icon="icon: home"></a></li>
@@ -18,7 +18,7 @@
 	<div class=" uk-grid-match uk-grid-small uk-text-center " uk-grid>
 		@foreach($row as $category)
 		<div class="uk-width-1-4@s">
-			<div class="uk-height-medium uk-flex uk-flex-center uk-flex-middle uk-background-cover uk-light" data-src="{{ asset($category->thumb) }}" uk-img>
+			<div class="uk-height-large uk-flex uk-flex-center uk-flex-middle uk-background-cover uk-light" data-src="{{ asset($category->thumb) }}" uk-img>
 				<h3><a href="#">{{ $category->name }}</a></h3>
 			</div>
 		</div>

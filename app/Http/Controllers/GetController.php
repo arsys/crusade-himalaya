1d87dc6f894b8595edd2d5815896f74621f67d75
Bantawa Pawan <rahunn3@gmail.com>
2018-12-28 13:10:49 +0545

    destination 2 package

--- app/Http/Controllers/GetController.php
+++ app/Http/Controllers/GetController.php
@@ -78,7 +78,7 @@ class GetController extends Controller
 		})->whereHas('region', function ($s) use ($region) {
 			$s->where('regions.slug', $region->slug);
 		})->get();  
-		dd($tours);
+		dd($query);
 		return view('frontend.pages.packages')        
 		->withCategory($category)
 		->withRegion($region)

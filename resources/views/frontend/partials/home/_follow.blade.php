d4ce60f200c3238e0cacfde8b2451580d2878c35
Bantawa Pawan <rahunn3@gmail.com>
2018-12-30 17:52:47 +0545

    instagram thumbnail

--- resources/views/frontend/partials/home/_follow.blade.php
+++ resources/views/frontend/partials/home/_follow.blade.php
@@ -7,7 +7,7 @@
         @foreach($instafeeds->chunk(10) as $row)
         <div class="follow uk-grid-collapse uk-child-width-expand@s uk-child-width-1-2   uk-child-height-1-2 " uk-grid="">
             @foreach($row as $feed)
-            <div><a href="#modal-media-image-{{ $loop->iteration }}" uk-toggle><img src="{{ $feed->thumb_link }}" alt="{{ $feed->caption }}" ></a></div>
+            <div><a href="#instagram-{{ $loop->iteration }}" uk-toggle><img src="{{ $feed->thumb_link }}" alt="{{ $feed->caption }}" ></a></div>
             @endforeach
         </div>
         @endforeach
@@ -25,7 +25,7 @@
             <div><a href="#modal-media-image10" uk-toggle><img src="https://source.unsplash.com/150x150/?chocolate" alt="" ></a></div>
         </div> --}}
         @foreach($instafeeds as $feed)
-        <div id="modal-media-image-{{  }}" class="uk-flex-top" uk-modal>
+        <div id="instagram-{{$loop->iteration}}" class="uk-flex-top" uk-modal>
             <div class="uk-modal-dialog  modal uk-width-auto uk-margin-auto-vertical">
                 <button class="uk-modal-close-outside" type="button" uk-close></button>
                 <div class="uk-grid-collapse" uk-grid>
@@ -39,10 +39,10 @@
                 </div>
             </div>
             @if($loop->iteration != $loop->first)
-            <a class="uk-position-center-left uk-position-small uk-slidenav-large previous" href="#modal-media-image-{{$loop->iteration-1 }}" uk-slidenav-previous uk-toggle></a>
+            <a class="uk-position-center-left uk-position-small uk-slidenav-large previous" href="#instagram-{{$loop->iteration-1 }}" uk-slidenav-previous uk-toggle></a>
             @endif
             @if($loop->iteration != $loo->last)
-            <a class="uk-position-center-right uk-position-small  uk-slidenav-large next" href="#modal-media-image-{{ $loop->iteration+1 }}" uk-slidenav-next uk-toggle></a>
+            <a class="uk-position-center-right uk-position-small  uk-slidenav-large next" href="#instagram-{{ $loop->iteration+1 }}" uk-slidenav-next uk-toggle></a>
             @endif
         </div>
         @endforeach

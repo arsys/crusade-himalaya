e02f9faa1785636637327794166ea4a03b28a8a7
Bantawa Pawan <rahunn3@gmail.com>
2018-12-30 13:46:48 +0545

    test

--- resources/views/frontend/tour/details.blade.php
+++ resources/views/frontend/tour/details.blade.php
@@ -21,32 +21,12 @@
 @stop
 @section('scripts')
 <script src="{{asset('js/product.js')}}">	</script>
-    <script type="text/javascript">
-        !function () {
-            "use strict";
-            $(document).ready(function () {
-                $("#fetch-dates").click(function (a) {
-                    a.preventDefault();
-                    var t = $("#tour-id").val(),
-                        e = $("#travel-year").val(),
-                        o = $("#travel-month").val();
-                    $('#ajax-action').hide(), $("#ajaxloader").show(), $.ajax({
-                        type: "GET",
-                        url: "/trip/fetch-departures",
-                        headers: {
-                            "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content")
-                        },
-                        data: {
-                            tour_id: t,
-                            year: e,
-                            month: o
-                        },
-                        success: function (a) {
-                            $(".ajaxloadmoredeparture").html(a), $("#ajaxloader").hide(), $('#ajax-action').show()
-                        }
-                    })
-                })
-            });
-        }();
-    </script>
+<script type="text/javascript">
+    !function () {
+        "use strict";
+        $(document).ready(function () {
+            console.log("ready");
+        });
+    }();
+</script>
 @stop

25ac7a120deda78e5b162a746a5a7ebd0763febf
Kajol2608 <rauniyarkajol2608@gmail.com>
2019-01-04 10:08:22 +0545

    travel style

--- resources/views/frontend/partials/home/_travel-style.blade.php
+++ resources/views/frontend/partials/home/_travel-style.blade.php
@@ -1,90 +1,74 @@
-
-<div class="uk-section uk-section-xsmall uk-section-default uk-margin-medium-top uk-margin-medium-bottom bgcolor-wrapper">
-    <div class="uk-container">
-        <h2 class="uk-h1  uk-text-center uk-margin-remove-bottom home-title uk-margin-large-bottom"><span>Travel Style</span></h2>
-        <p class="uk-text-center uk-margin-remove-top home-heading-secondary uk-margin-large-bottom  " id="home-sub-title"><span> Don't Miss the best things, keep your eyes open</span></p>
-        <div class="uk-position-relative uk-visible-toggle uk-light" uk-slider>
-            @if($categories->count())
-            <ul class="uk-slider-items uk-child-width-1-2 uk-child-width-1-4@m uk-grid">
+<div class="uk-section uk-section-xsmall uk-section-default uk-margin-medium-top uk-margin-medium-bottom">
+    <h2 class="uk-h1  uk-text-center uk-margin-remove-bottom home-title uk-margin-large-bottom"><span>Travel Style</span></h2>
+    <p class="uk-text-center uk-margin-remove-top home-heading-secondary uk-margin-large-bottom  " id="home-sub-title"><span> Don't Miss the best things, keep your eyes open</span></p>
+    <div class="uk-position-relative uk-visible-toggle uk-light" uk-slider>
+     <!--    <ul class="uk-slider-items uk-child-width-1-2 uk-child-width-1-3@s uk-child-width-1-5@m">
+         <li>
+             <img src="https://source.unsplash.com/320x450/?trekking" alt="">
+             <div class="uk-position-bottom  travel"><h1>1</h1></div>
+         </li>
+         <li>
+             <img src="https://source.unsplash.com/320x450/?trekking" alt="">
+             <div class="uk-position-bottom  travel"><h1>2</h1></div>
+         </li>
+         <li>
+             <img src="https://source.unsplash.com/320x450/?trekking" alt="">
+             <div class="uk-position-bottom  travel"><h1>3</h1></div>
+         </li>
+         <li>
+             <img src="https://source.unsplash.com/320x450/?trekking" alt="">
+             <div class="uk-position-bottom  travel"><h1>4</h1></div>
+         </li>
+         <li>
+             <img src="https://source.unsplash.com/320x450/?kathmandu" alt="">
+             <div class="uk-position-bottom  travel"><h1>5</h1></div>
+         </li>
+         <li>
+             <img src="https://source.unsplash.com/320x450/?mountain" alt="">
+             <div class="uk-position-bottom  travel"><h1>6</h1></div>
+         </li>
+         <li>
+             <img src="https://source.unsplash.com/320x450/?india" alt="">
+             <div class="uk-position-bottom  travel"><h1>7</h1></div>
+         </li>
+         <li>
+             <img src="https://source.unsplash.com/320x450/?nepal" alt="">
+             <div class="uk-position-bottom  travel"><h1>8</h1></div>
+         </li>
+         <li>
+             <img src="https://source.unsplash.com/320x450/?climbing" alt="">
+             <div class="uk-position-bottom  travel"><h1>9</h1></div>
+         </li>
+         <li>
+             <img src="https://source.unsplash.com/320x450/?everest" alt="">
+             <div class="uk-position-bottom  travel"><h1>10</h1></div>
+         </li>
+     </ul> -->
+       @if($categories->count())
+            <ul class="uk-slider-items uk-child-width-1-2 uk-child-width-1-3@s uk-child-width-1-5@m">
                 @foreach($categories as $category)
                 <li>
                     <div class="uk-panel">
                         <a href="{{ route('fetchByCategory',$category->slug) }}">
                             <img class="home-img" src="{{ asset($category->thumb) }}" alt="{{ $category->name }}">
                         </a>
-                        <div class="uk-position-center uk-panel" id="activity-title">
-                            <h2 class="travel-heading"><a href="{{ route('fetchByCategory',$category->slug) }}">{{ $category->name }}</a></h2>
+                        <div class="uk-position-bottom  travel uk-animation-toggle" id="activity-title">
+                            <div class="uk-flex uk-flex-column  uk-animation-slide-top-medium ">
+                                <span class="travel-heading "><a href="{{ route('fetchByCategory',$category->slug) }}">{{ $category->name }}</a></span>
+                                <span class="travel-view"><a href="">View All tours </a></span>
+                            </div>
+
                         </div>
                     </div>
                 </li>
                 @endforeach
             </ul>
-            @else
-         <!--    <ul class="uk-slider-items uk-child-width-1-2 uk-child-width-1-4@m uk-grid">
-             <li>
-                 <div class="uk-panel">
-                     <a href="/product"><img src="https://source.unsplash.com/320x450/?trekking" alt=""></a>
-                     <div class="uk-position-center uk-panel" id="activity-title"><h3>Trekking</h2></div>
-                 </div>
-             </li>
-             <li>
-                 <div class="uk-panel">
-                     <a href="/product"><img src="https://source.unsplash.com/320x450/?surf" alt=""></a>
-                     <div class="uk-position-center uk-panel" id="activity-title"><h2>Surfing</h2></div>
-                 </div>
-             </li>
-             <li>
-                 <div class="uk-panel">
-                     <a href="/product"><img src="https://source.unsplash.com/320x450/?ski" alt=""></a>
-                     <div class="uk-position-center uk-panel" id="activity-title"><h2>Sking</h2></div>
-                 </div>
-             </li>
-             <li>
-                 <div class="uk-panel">
-                     <a href="/product"><img src="https://source.unsplash.com/320x450/?africa-safari" alt=""></a>
-                     <div class="uk-position-center uk-panel" id="activity-title"><h2>Safari</h2></div>
-                 </div>
-             </li>
-             <li>
-                 <div class="uk-panel">
-                     <a href="/product"><img src="https://source.unsplash.com/320x450/?fishing" alt=""></a>
-                     <div class="uk-position-center uk-panel" id="activity-title"><h2>Fishing</h2></div>
-                 </div>
-             </li>
-             <li>
-                 <div class="uk-panel">
-                     <a href="/product"><img src="https://source.unsplash.com/320x450/?rock-climb" alt=""></a>
-                     <div class="uk-position-center uk-panel" id="activity-title"><h2>Climbing</h2></div>
-                 </div>
-             </li>
-             <li>
-                 <div class="uk-panel">
-                     <a href="/product"><img src="https://source.unsplash.com/320x450/?god" alt=""></a>
-                     <div class="uk-position-center uk-panel" id="activity-title"><h2>7</h2></div>
-                 </div>
-             </li>
-             <li>
-                 <div class="uk-panel">
-                     <a href="/product"><img src="https://source.unsplash.com/320x450/?temple" alt=""></a>
-                     <div class="uk-position-center uk-panel" id="activity-title"><h2>Culture</h2></div>
-                 </div>
-             </li>
-             <li>
-                 <div class="uk-panel">
-                     <a href="/product"><img src="https://source.unsplash.com/320x450/?yoga"alt=""></a>
-                     <div class="uk-position-center uk-panel" id="activity-title"><h2>Retreat</h2></div>
-                 </div>
-             </li>
-             <li>
-                 <div class="uk-panel">
-                     <a href="/product"><img src="https://source.unsplash.com/320x450/?annapurna" alt=""></a>
-                     <div class="uk-position-center uk-panel" id="activity-title"><h2>Nature</h2></div>
-                 </div>
-             </li>
-         </ul> -->
-            @endif
-            <a class="uk-position-center-left uk-position-small uk-slidenav-large" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
-            <a class="uk-position-center-right uk-position-small uk-slidenav-large " href="#" uk-slidenav-next uk-slider-item="next"></a>
-        </div>
+          @endif
+
+        <a class="uk-position-center-left uk-position-small uk-slidenav-large next" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+        <a class="uk-position-center-right uk-position-small uk-slidenav-large next" href="#" uk-slidenav-next uk-slider-item="next"></a>
+
+
     </div>
+  
 </div>

6867a071f9ccc6b994cd39f576f4077e15774ef4
Bantawa Pawan <rahunn3@gmail.com>
2019-01-06 12:06:19 +0545

    about content

---
+++ resources/views/frontend/partials/about/_content.blade.php
@@ -0,0 +1,55 @@
+<div class="uk-section uk-section-small uk-section-default">
+	<div class="uk-container">
+		<div class="text-align-left uk-margin-medium-top" uk-grid>
+			<div class="uk-width-1-1" >
+				<p><strong>Why Nepal?</strong></p>
+
+				<p>Nepal as a destination often surpasses a visitor&#39;s expectations &ndash; the lush green plains to the world&#39;s tallest mountain, &nbsp;dense jungles to arid valleys, glacial lakes to raging rivers, diverse culture and their lives, to experience Nepal is one of the most exhilarating experience one can indulge in. &nbsp;&nbsp;</p>
+
+				<p>Although constituting &nbsp;a reasonably finite geographical area, the experiences one can indulge in this nation are infinite. Paradise like landscapes, pristine peaks, a day of solitude and yet the next day with adventure, the intertwining of history and modernity &ndash; these are only some of the facets of Nepal. To truly understand and fully comprehend the vastness of Nepal, one has to visit the nation.</p>
+
+				<p><strong>Why Crusade Himalaya?</strong></p>
+
+				<p>Crusade Himalaya is a unique and independent travel partner; we are a boutique travel studio who wish to offer you, our Atithis (revered guests), enriching and fulfilling experience of their Nepal visit. We wish to Empower, Educate, and Encourage you to have real-life experiences as you set out to discover the timeless wonders of Nepal.</p>
+
+				<p><strong>How we work?</strong></p>
+
+				<p>At Crusade Himalaya, we offer highly personalized travel plans and arrangements to ensure your satisfaction. Putting comfort and pleasure above anything else, we wish to earn your smiles. From the moment you land in Nepal, till the time we bid our goodbyes, the team of Crusade Himalaya will continuously strive to ensure your experience is highly memorable.</p>
+
+				<p><strong><em>Our team will be your perfect travel friend;<br />
+					a knowledgeable guide when you wonder the timeless alleys of the Durbar Square,<br />
+					a trusted friend when you trek the heavenly trails leading to the Himalayas,<br />
+					a perfect organizer who will plan your trip as per your wish,<br />
+				and a hospitable friend who will ensure your comfort during your entire stay.</em></strong></p>
+
+				<p>&nbsp;</p>
+
+				<p><strong>What makes Crusade Himalaya different?</strong></p>
+
+				<p>Besides <strong>&#39;knowledge infused with deep passion for meaningful travel&#39;</strong>, what sets Crusade Himalayas apart is their unique working style. We are an independent travel partner, and will therefore organize your trip as per your expectations, with our recommendations.</p>
+
+				<p>At Crusade Himalayas, we <strong>give the power of choice to you</strong>. <em>You decide where you want to go, what you want to see, how you want to get there, we are simply your travel friends.</em></p>
+
+				<p><strong>What is our mission?</strong></p>
+
+				<p>Our mission is simple, it is <strong>to make you happy</strong>. To do so, we will take you across many of the delights Nepal has to offer, introduce you to different cultures, and all the while ensuring your comfort and satisfaction.</p>
+
+				<p><strong>Your adventure awaits you! </strong><br />
+					Get in touch with us, and plan your perfect holiday &ndash; an unforgettable holiday filled with<br />
+					<strong>adventure, excitement, and memories.</strong></p>
+
+					<p><strong>Our Three E&#39;s Explained:</strong></p>
+
+					<ul>
+						<li><strong>Empower:</strong> Empower the traveler to make their own decisions related to their travel.</li>
+						<li><strong>Educate:</strong> Our website also offers important information concerning travelling in Nepal. One can simply read away, and visit Nepal upon their own. You may also address your questions regarding Nepal to us.</li>
+						<li><strong>Encourage</strong>: To travel is a beautiful thing, it broadens one&#39;s horizons, at the same time makes them humble. Therefore, we encourage all to travel.</li>
+					</ul>
+
+					<p>&nbsp;</p>
+
+				</div>
+			</div>
+		</div>
+	</div>
+
--- resources/views/frontend/partials/about/_image.blade.php
+++ resources/views/frontend/partials/about/_image.blade.php
@@ -1,7 +1,10 @@
 <section class="uk-container-expand about" uk-grid>
 	<div class="uk-height-large  uk-width-*@s  uk-background-cover uk-light page-cover" data-src="https://source.unsplash.com/1200x800/?arts" uk-img>
 		<h1 class="page-heading uk-margin-large-left">About us</h1>
-		<h3 class="page-subheading uk-margin-large-left uk-margin-top uk-margin-bottom">Lorem ipsum dolor sit amet.</h3>
+		<h3 class="page-subheading uk-margin-large-left uk-margin-top uk-margin-bottom">
+			Nepal isn't a destination, it is an experience; 
+			an experience beyond the ordinary.
+		</h3>
 		<div class="breadcrumb ">
 			<ul class="uk-breadcrumb" id="breadcrumb">
 				<li><a href="/" uk-icon="icon: home" ></a></li>

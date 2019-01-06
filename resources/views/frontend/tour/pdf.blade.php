87952f3926897b36b675288e844fdcb0f7636a59
Bantawa Pawan <rahunn3@gmail.com>
2019-01-06 17:18:32 +0545

    download pdf

--- resources/views/frontend/tour/pdf.blade.php
+++ resources/views/frontend/tour/pdf.blade.php
@@ -13,23 +13,19 @@
 			<div class="tab-pane active" id="overview">
 				<div class="row">
 					<div class="col-sm-6">
-						<h3>Rediscover lost Sangrila</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id aut similique voluptas! Tenetur aperiam alias, officiis, perferendis accusamus, sint, error possimus quas repellendus facilis asperiores dolorem. Nostrum maiores laudantium
-							possimus consequatur quam. Dolores placeat laudantium similique, beatae animi, vel reprehenderit! Quisquam veniam laudantium ullam eaque! Error ex cumque iure aliquam commodi voluptatibus, animi quam quas maiores, deserunt rerum, provident
-						consequuntur!</p>
-						<p>Dolor sit amet, consectetur adipisicing elit. Incidunt consequatur iusto odio quis magnam, aut assumenda ipsa magni ea, veritatis, nostrum rerum necessitatibus excepturi eos et nemo iste? Illo temporibus mollitia ducimus aspernatur numquam,
-						sint sunt consequatur rerum aliquam odio!</p>
-						<p>Consectetur adipisicing elit. Debitis natus eum autem nisi cumque optio. Quis, necessitatibus laboriosam, alias, ea aut atque facere fugiat iusto tenetur minima itaque vero aperiam! Veniam exercitationem, distinctio laborum magnam rerum
-						quia et commodi hic quis suscipit! Molestias, eaque. Incidunt, a, molestias! Veniam, nobis, recusandae.</p>
-						<blockquote class="with-icon">
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis laboriosam repudiandae eligendi illum vero cum corrupti impedit nihil, aliquid. Veniam!
-							<footer>Someone famous in <cite title="Source Title">Source Title</cite>
-							</footer>
-						</blockquote>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, deleniti optio similique nobis.
-						</p>
-
+						<h3>{{ $tour->title }}</h3>
+						<h4>What's included ?</h4>
+						<ul class="included">
+							@foreach($tour->includes as $included)
+							<li>{{$included->name}}</li>
+							@endforeach
+						</ul>
+						<h4>What's excluded ?</h4>
+						<ul class="excluded">
+							@foreach($tour->excludes as $excluded)
+							<li>{{$excluded->name}}</li>
+							@endforeach
+						</ul> 
 					</div>
 					<div class="col-sm-6">
 						<div class="border-box">
@@ -40,56 +36,42 @@
 									<span class="icon-road-sign"></span>
 									<div class="detail">
 										<div class="title">Trip profile</div>
-										<div class="desc">21 Day Trip 14 days point-to-point trekdiv nights</div>
+										<div class="desc">{{ $tour->days }} Day {{ $tour->title }}</div>
 									</div>
 								</li>
 								<li>
 									<span class="icon-dollar"></span>
 									<div class="detail">
 										<div class="title">Cost</div>
-										<div class="desc">USD 422</div>
+										<div class="desc">USD {{ $tour->price }}</div>
 									</div>
 								</li>
 								<li>
 									<span class="icon-camp-fire"></span>
 									<div class="detail">
 										<div class="title">Type</div>
-										<div class="desc">Camping</div>
-									</div>
-								</li>
-								<li>
-									<span class="icon-barcode"></span>
-									<div class="detail">
-										<div class="title">Trip Code</div>
-										<div class="desc">AD 23</div>
+										<div class="desc">{{ $tour->category->name }}</div>
 									</div>
 								</li>
 								<li>
 									<span class="icon-calendar"></span>
 									<div class="detail">
 										<div class="title">Trek Days</div>
-										<div class="desc">18</div>
+										<div class="desc">{{ $tour->days }}</div>
 									</div>
 								</li>
 								<li>
 									<span class="icon-door-tag "></span>
 									<div class="detail">
 										<div class="title">Accomodation</div>
-										<div class="desc">6 Night hotel, 3 night Tea House</div>
+										<div class="desc">{{ $tour->accommodation->name }}</div>
 									</div>
 								</li>
 								<li>
-									<span class="icon-home"></span>
+									<span class="icon-bowl"></span>
 									<div class="detail">
-										<div class="title">Trek Accomodation</div>
-										<div class="desc">6 Night hotel, 3 night Tea House</div>
-									</div>
-								</li>
-								<li>
-									<span class="icon-bus"></span>
-									<div class="detail">
-										<div class="title">Transportation</div>
-										<div class="desc">Bus, Jeep</div>
+										<div class="title">Meal</div>
+										<div class="desc">{{ $tour->meal->plan }}</div>
 									</div>
 								</li>
 							</ul>

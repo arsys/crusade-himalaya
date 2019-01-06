611089308b8e348b1abda187eaeadf0c402dff8e
Bantawa Pawan <rahunn3@gmail.com>
2019-01-06 18:49:57 +0545

    Download pdf

--- resources/views/frontend/tour/pdf.blade.php
+++ resources/views/frontend/tour/pdf.blade.php
@@ -22,6 +22,7 @@
 		<div class="tab-panel active" id="overview">
 			<div class="row">
 				<div class="col-sm-6 col-lg-6 col-md-6">
+					<h3>{{ $tour->days }} Days {{ $tour->title }}</h3>
 					<div class="border-box">
 						<br>
 						<div class="box-title">Trip Overview</div>
@@ -72,7 +73,6 @@
 					</div>
 				</div>
 				<div class="col-sm-6 col-lg-6 col-md-6">
-					<h3>{{ $tour->days }} Days {{ $tour->title }}</h3>
 					<h4>What's included ?</h4>
 					<ul class="included">
 						@foreach($tour->includes as $included)

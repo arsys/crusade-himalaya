50954257a15e42b908db4dacefb98a1923fbff62
Bantawa Pawan <rahunn3@gmail.com>
2019-01-07 13:50:05 +0545

    refer a friend

--- resources/views/frontend/tour/partials/modal/_refer.blade.php
+++ resources/views/frontend/tour/partials/modal/_refer.blade.php
@@ -2,7 +2,7 @@
 	<div class="uk-modal-dialog uk-margin-auto-vertical">
 		<button class="uk-modal-close-default" type="button" uk-close></button>
 		<div class="uk-modal-header">
-			<h4 class="uk-modal-title">Refer a friend</h4>
+			<h5 class="uk-modal-title">Refer a friend</h5>
 		</div>
 		<div class="uk-modal-body">
 			<form action="#" id="refer-friend" class="uk-form-stacked">
@@ -21,25 +21,39 @@
 						<div class="uk-margin">
 							<div class="uk-width-1-1@s">
 								<label class="uk-form-label" for="form-stacked-text">Email</label>
-								<input class="uk-input" type="email" name="fname">
+								<input class="uk-input" type="email" name="myEmail">
 							</div>
 						</div>
 						<div class="uk-margin">
-							<textarea class="uk-textarea" rows="5" placeholder="Comment" name="comment"></textarea>
+							<textarea class="uk-textarea" rows="5" placeholder="Comment" name="comment" style="resize: none;"></textarea>
 						</div>
 						<div class="uk-margin uk-grid-small uk-child-width-auto uk-grid">							
 							<label><input class="uk-checkbox" type="checkbox" name="copy" value="1"> Send me a copy</label>
 						</div>
 					</div>
 					<div class="uk-width-1-2">
+						<h6>Send to</h6>
+						<div class="uk-margin">
+							<div class="uk-width-1-1">
+								<label class="uk-form-label" for="form-stacked-text">To:</label>
+								<input class="uk-input" placeholder="Enter email 1 (Required)" type="email" name="sendTo1" required>
+							</div>
+						</div>
 						<div class="uk-margin">
 							<div class="uk-width-1-1">
 								<label class="uk-form-label" for="form-stacked-text">Email</label>
-								<input class="uk-input" type="email" name="fname">
+								<input class="uk-input" placeholder="Enter email 2" type="email" name="sendTo2">
+							</div>
+						</div>
+						<div class="uk-margin">
+							<div class="uk-width-1-1">
+								<label class="uk-form-label" for="form-stacked-text">Email</label>
+								<input class="uk-input" placeholder="Enter email 3" type="email" name="sendTo3">
 							</div>
 						</div>
 					</div>
 				</div>
+				<button class="uk-button uk-button-primary send-refer uk-width-1-1" type="submit">Send</button>
 			</form>
 		</div>
 	</div>

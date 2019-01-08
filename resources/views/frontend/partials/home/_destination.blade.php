133568f256900b739cacfc2be7155c499b2d0965
Kajol2608 <rauniyarkajol2608@gmail.com>
2019-01-08 13:47:31 +0545

    hash

--- resources/views/frontend/partials/home/_destination.blade.php
+++ resources/views/frontend/partials/home/_destination.blade.php
@@ -3,11 +3,11 @@
         <div class="uk-container">
             <h2 class="uk-h1  uk-text-center uk-margin-remove-bottom home-title uk-margin-large-bottom"><span>DESTINATION</span></h2>
             <p class="uk-text-center uk-margin-remove-top home-heading-secondary uk-margin-large-bottom  " id="home-sub-title"><span>Ready to fall for nepalese charm ? start here </span></p>
-            <div class="uk-grid-collapse uk-child-width-1-2  " uk-grid>
-                <div class="uk-first-column">
+            <div class="uk-grid-collapse uk-child-width-1-2 uk-child-height-1-2 uk-height-match uk-grid" uk-grid>
+                <div class="uk-first-column uk-padding-xsmall">
                     @foreach($odds as $odd)
                     @if($loop->iteration % 2 == 0)
-                    <div class="destination">
+                    <div class="destination ">
                         <a href="{{ route('fetchByRegion',$odd->slug) }}"><img src="{{ asset($odd->thumb) }}" alt="" ></a>
                         <div class="dest-topic">
                             <a href="{{ route('fetchByRegion',$odd->slug) }}">
@@ -17,11 +17,11 @@
                     </div>
                     @endif
                     @endforeach
-                    <div class="uk-child-width-expand@s uk-text-center  uk-grid-collapse" uk-grid>
+                    <div class="uk-child-width-expand@s uk-text-center uk-height-match uk-grid   uk-grid-collapse" uk-grid>
                         @foreach($odds as $odd)
                         @if($loop->iteration % 2 != 0)
-                        <div class="{{$loop->first ? 'uk-first-column': '' }} uk-panel uk-padding-xsmall destination">
-                            <a href="{{ route('fetchByRegion',$odd->slug) }}"><img src="{{ asset($odd->thumb) }}" alt="" ></a>
+                        <div class="{{$loop->first ? 'uk-first-column': '' }}   destination uk-padding-xsmall  ">
+                            <a href="{{ route('fetchByRegion',$odd->slug) }}"><img src="{{ asset($odd->thumb) }}" alt=""></a>
                             <div class="dest-topic">
                                 <a href="{{ route('fetchByRegion',$odd->slug) }}">
                                     <div class="dest-title">{{ $odd->name }}</div>
@@ -34,11 +34,11 @@
                 </div>
 
 
-                <div>
-                    <div class="uk-child-width-expand@s uk-text-center uk-grid-collapse uk-padding-xsmall" uk-grid>
+                <div class="uk-first-column uk-padding-xsmall">
+                    <div class="uk-child-width-expand@s uk-text-center uk-grid uk-height-match  uk-grid-collapse uk-padding-xsmall" uk-grid>
                         @foreach($evens as $even)
                         @if($loop->iteration % 2 != 0)
-                        <div class="{{ $loop->first ? 'uk-first-column' : '' }} uk-panel destination">
+                        <div class="{{ $loop->first ? 'uk-first-column' : '' }} uk-panel  destination ">
                             <a href="{{ route('fetchByRegion',$even->slug) }}"><img src="{{ asset($even->thumb) }}" alt=""></a>
                             <div class="dest-topic">
                                 <a href="{{ route('fetchByRegion',$even->slug) }}">
@@ -52,7 +52,7 @@
                     @foreach($evens as $even)
                     @if($loop->iteration % 2 == 0)
                     <div class="destination">
-                        <a href="{{ route('fetchByRegion',$even->slug) }}"><img src="{{ asset($even->thumb) }}" alt="" ></a>
+                        <a href="{{ route('fetchByRegion',$even->slug) }}"><img src="{{ asset($even->thumb) }}" alt=""></a>
                         <div class="dest-topic">
                             <a href="{{ route('fetchByRegion',$even->slug) }}">
                                 <div class="dest-title">{{ $even->name }}</div>

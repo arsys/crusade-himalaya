a1a4a7b5a5e19bce8e961a4ff24a31ee528651e8
Kajol2608 <rauniyarkajol2608@gmail.com>
2019-01-10 14:56:09 +0545

    assets minified

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,5 +15,6 @@ const mix = require('laravel-mix');
 mix.browserSync('localhost:8000')
 .js('resources/js/app.js', 'public/js')
 .js('resources/js/product.js', 'public/js')
+.js('resources/js/navbar.js', 'public/js')
 .sass('resources/sass/app.scss', 'public/css')
 .sass('resources/sass/backend/style.scss','public/assets/backend/css/');

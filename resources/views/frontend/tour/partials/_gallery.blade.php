c51dcf5a03fb0492f7efd4a1fc52623d00375b20
Bantawa Pawan <rahunn3@gmail.com>
2019-01-11 16:48:18 +0545

    gallery fixed

--- resources/views/frontend/tour/partials/_gallery.blade.php
+++ resources/views/frontend/tour/partials/_gallery.blade.php
@@ -1,11 +1,11 @@
-<div class="uk-child-width-1-3@m uk-grid-small" uk-grid uk-lightbox="animation: slide">
+<div class="uk-child-width-1-3@m " uk-grid uk-lightbox="animation: slide">
 	
-    <div>
-    	@foreach($tour->slides as $slide)
-        <a class="uk-inline" href="{{asset($slide->path)}}" data-caption="{{ $slide->name }}">
-            <img src="{{ asset($slide->thumb) }}" alt="{{ $slide->name }}">
-        </a>
-        @endforeach
-    </div>
-   
+	@foreach($tour->slides as $slide)
+	<div>
+		<a class="uk-inline" href="{{asset($slide->path)}}" data-caption="{{ $slide->name }}">
+			<img src="{{ asset($slide->thumb) }}" alt="{{ $slide->name }}">
+		</a>
+	</div>
+	@endforeach
+	
 </div>

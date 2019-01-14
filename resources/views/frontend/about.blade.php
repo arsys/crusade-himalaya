f43e81108a004398c9caad3be96a2258c97242ac
Kajol2608 <rauniyarkajol2608@gmail.com>
2019-01-14 10:51:58 +0545

    js

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -7,9 +7,9 @@
 @include ('frontend.partials.about._content')
 <!-- about us end-->
 <!-- guide start -->
-@include ('frontend.partials.about._guide')
+{{-- @include ('frontend.partials.about._guide') --}}
 <!-- guide end -->
-<!-- team start -->
-@include ('frontend.partials.about._team') 
+{{-- <!-- team start -->
+@include ('frontend.partials.about._team')  --}}
 <!-- team end -->
 @stop

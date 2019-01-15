e365a2c67d9e23f1b04792142c9687c6362f301d
Kajol2608 <rauniyarkajol2608@gmail.com>
2019-01-15 11:18:19 +0545

    about page

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -6,28 +6,11 @@
 <!-- about us start-->
 @include ('frontend.partials.about._content')
 <!-- about us end-->
+
+<!-- team start -->
+@include ('frontend.partials.about._teams') 
+<!-- team end -->
 <!-- guide start -->
 @include ('frontend.partials.about._guide')
 <!-- guide end -->
-<!-- team start -->
-@include ('frontend.partials.about._team') 
-<!-- team end -->
-@stop
-@section('scripts')
-<script>
-(function ($) {
-(function ($) {
-    $(document).ready(function(){       
-
-        $(window).scroll(function () {
-            if ($(this).scrollTop() > 500) {
-                $("#team-wrapper").removeClass("animated fadeInDown");
-                $("#team-wrapper").fadeOut(900,'linear');
-                
-            } else {
-                $("#team-wrapper").addClass("animated fadeIn").show();
-            }
-        });
-    });
-</script>
 @stop

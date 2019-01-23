ad57cabea848bd263a1f104f6a39ff7c8e71074e
Bantawa Pawan <rahunn3@gmail.com>
2019-01-23 14:51:08 +0545

    sitemap

--- resources/views/frontend/page.blade.php
+++ resources/views/frontend/page.blade.php
@@ -3,7 +3,7 @@
 {{-- image start --}}
 <section class="uk-container-expand about" uk-grid>
 	<div class="uk-height-large  uk-width-*@s  uk-background-cover uk-light page-cover" data-src="https://source.unsplash.com/1200x800/?rose" uk-img>
-		<h1 class="page-heading uk-margin-large-left">{{ $page->heading }}</h1>
+		<h1 class="page-heading uk-margin-large-left">{{ $page->title }}</h1>
 		<p class="page-subheading uk-margin-large-left uk-margin-top uk-margin-bottom">
 			{{ $page->subheading }}
 		</p>

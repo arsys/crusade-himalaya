0a64a7b7667a06f3fc673cdc0f17f3298d143b5f
Bantawa Pawan <rahunn3@gmail.com>
2019-01-25 17:26:29 +0545

    booking

--- resources/views/frontend/tour/book/step1.blade.php
+++ resources/views/frontend/tour/book/step1.blade.php
@@ -41,7 +41,7 @@
     <div class="uk-grid-small" uk-grid>
       <div class="uk-width-1-2@s">
         <label class="uk-form-label">Start Date</label>
-        <input class="uk-input" type="text" name="date" placeholder="{!! $_GET['date'] !!}" value="{!! $_GET['date'] !!}"  disabled>
+        <input class="uk-input" type="text" name="date" placeholder="{{ date("jS M, Y", strtotime($data['date']))}}" value="{{ $data['date'] }}"  disabled>
       </div>
       <div class="uk-width-1-2@s">
         <div class="uk-margin">

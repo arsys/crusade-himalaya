35917a05c7bf496a15a07fe61319473ac0ce724c
Bantawa Pawan <rahunn3@gmail.com>
2019-01-25 20:06:59 +0545

    Booking email

--- routes/web.php
+++ routes/web.php
@@ -121,7 +121,7 @@ Route::prefix('trip')->name('trip.')->group(function () {
 	Route::post('join/{slug}/setp-1','GetController@stepOne')->name('stepOne'); //shows step 1 form
 	Route::post('join/{slug}/step-2','GetController@stepTwo')->name('stepTwo'); //shows step 2 form
 	Route::post('join/{slug}/step-3', 'GetController@stepThree')->name('stepThree'); //poost step 2 form
-	Route::get('join/{slug}/step-3','FrontendController@getThankyou')->name('thankyou'); //show step 3 thank you
+	Route::get('join/{slug}/step-3','GetController@getThankyou')->name('thankyou'); //show step 3 thank you
 });
 Route::prefix('ajax')->group(function () {
 	Route::get('fetch-departures','FrontendController@ajaxsearchdeparture')->name('departure');

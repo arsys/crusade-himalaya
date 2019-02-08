25378ffcc85224fee3d00c74847417670b3c8478
Bantawa Pawan <rahunn3@gmail.com>
2019-02-08 12:28:54 +0545

    calender dynymic

--- resources/views/frontend/tour/partials/modal/_enquiry.blade.php
+++ resources/views/frontend/tour/partials/modal/_enquiry.blade.php
@@ -2,7 +2,7 @@
 	<ul class="uk-list">
 	</ul>
 </div>
-<h3 >Enquiry Form</h3>
+<h3 >Quick Enquiry {{ $tour->title }}</h3>
 <form action="{{ route('frontend-postEnquiry') }}" method="POST" id="quick-enquiry">
 	<input type="hidden" id="tour_id" value="{{ $tour->id }}" name="tour_id">
 	<div class="uk-margin">

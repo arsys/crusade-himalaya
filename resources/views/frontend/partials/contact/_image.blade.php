45fc10428900296c97d447db5245938be92852f8
Bantawa Pawan <rahunn3@gmail.com>
2019-02-11 12:30:04 +0545

    image

--- resources/views/frontend/partials/contact/_image.blade.php
+++ resources/views/frontend/partials/contact/_image.blade.php
@@ -1,5 +1,5 @@
 <section class="uk-container-expand">
-	<div class="uk-height-large uk-flex uk-flex-center uk-flex-middle uk-background-cover uk-light" data-src="https://source.unsplash.com/1200x800/?arts" uk-img>
+<div class="uk-height-large uk-flex uk-flex-center uk-flex-middle uk-background-cover uk-light" data-src="{{asset('img/contact.jpg')}}" uk-img>
 	<div class="uk-overlay-primary uk-position-cover overlay"></div>
 	<h1 class="main-heading">Contact us</h1>
 		<div class="contact-breadcrumb ">
@@ -7,6 +7,6 @@
 				<li><a href="/" uk-icon="icon: home" ></a></li>
 				<li class="uk-disabled"><a href="{{ url()->current() }}">Contact Us</a></li>
 			</ul>
-		</div>	
+		</div>
 	</div>
 </section>

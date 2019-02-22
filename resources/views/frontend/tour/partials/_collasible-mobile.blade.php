dc0ee32693c90dae3f8296287087d47562f5d1f4
Bantawa Pawan <rahunn3@gmail.com>
2019-02-22 14:35:24 +0545

    per person

--- resources/views/frontend/tour/partials/_collasible-mobile.blade.php
+++ resources/views/frontend/tour/partials/_collasible-mobile.blade.php
@@ -2,11 +2,21 @@
     <div class="uk-card uk-card-default  uk-card-body ">
         <div class="uk-text-center" uk-grid>
             <div class="uk-width-1-2">
+                {{--
                 <div class="mobile-days uk-flex uk-flex-row">
                     <span class="mobile-count uk-align-center">{{ $tour->days }} Days</span>
                 </div>
                 <div class="mobile-duration uk-flex uk-flex-row ">
                     <span class="price-details  uk-align-center">USD <sup>$</sup>{{ $tour->price }}</span>
+                </div> --}}
+                <div class="uk-flex uk-flex-row">
+                    <span class="uk-align-center">{{ $tour->days }} Days</span>
+                </div>
+                <div class="uk-flex uk-flex-row">
+                    <span class="uk-align-center">USD <sup>$</sup>{{ $tour->price }}</span>
+                </div>
+                <div class="uk-flex uk-flex-row">
+                    <span class="uk-align-center">per person</span>
                 </div>
             </div>
             <div class="uk-width-1-2 price-divide">
@@ -14,17 +24,17 @@
                     <li>
                         <div class="uk-grid-small uk-child-width-expand uk-flex-nowrap uk-flex-middle uk-grid" uk-grid="">
                             <div class="uk-width-auto uk-first-column">
-                                <i class="fas fa-info"></i>   
+                                <i class="fas fa-info"></i>
                             </div>
                             <div {{-- uk-scrollspy-nav="closest: a; scroll: true; offset: 40" uk-nav --}}>
-                                <a href="#mob-enquiry" class="uk-link-text uk-link-reset">Quick Enquiry</a> 
+                                <a href="#mob-enquiry" class="uk-link-text uk-link-reset">Quick Enquiry</a>
                             </div>
                         </div>
                     </li>
                     <li>
                         <div class="uk-grid-small uk-child-width-expand uk-flex-nowrap uk-flex-middle uk-grid" uk-grid="">
                             <div class="uk-width-auto uk-first-column">
-                                <i class="fas fa-envelope"></i>   
+                                <i class="fas fa-envelope"></i>
                             </div>
                             <div>
                                 <a href="#modal-refer-friend" class="uk-link-text uk-link-reset" uk-toggle>Email a friend</a>
@@ -46,7 +56,7 @@
                 </ul>
             </div>
         </div>
-    </div>    
+    </div>
 </div>
 
 <div class="uk-container uk-padding-large uk-hidden@l">
@@ -54,39 +64,38 @@
         <li class=" uk-box-shadow-small uk-padding uk-background-default">
             <a class="uk-accordion-title" href="#">Overview</a>
             <div class="uk-accordion-content">
-                @include ('frontend.tour.partials._overview')
+    @include ('frontend.tour.partials._overview')
             </div>
         </li>
-        <li class="uk-box-shadow-small uk-padding uk-background-default " >
+        <li class="uk-box-shadow-small uk-padding uk-background-default ">
             <a class="uk-accordion-title" href="#">Inclusion</a>
             <div class="uk-accordion-content">
-                @include ('frontend.tour.partials._include')
+    @include ('frontend.tour.partials._include')
             </div>
         </li>
         @if($tour->itinerary->count() > 0)
-        <li class="uk-box-shadow-small uk-padding uk-background-default" >
+        <li class="uk-box-shadow-small uk-padding uk-background-default">
             <a class="uk-accordion-title" href="#">Itinerary</a>
             <div class="uk-accordion-content">
-                 @include ('frontend.tour.partials._itinerary')
+    @include ('frontend.tour.partials._itinerary')
             </div>
         </li>
-        @endif
-        @if($tour->departure->count() > 0)
-        <li class="uk-box-shadow-small uk-padding uk-background-default" >
+        @endif @if($tour->departure->count() > 0)
+        <li class="uk-box-shadow-small uk-padding uk-background-default">
             <a class="uk-accordion-title" href="#">Tour Dispatch</a>
             <div class="uk-accordion-content">
-                @include ('frontend.tour.partials._price-mobile')
+    @include ('frontend.tour.partials._price-mobile')
             </div>
         </li>
         @endif
         <li class="uk-box-shadow-small uk-padding uk-background-default">
             <a class="uk-accordion-title" href="#">Gallery</a>
             <div class="uk-accordion-content">
-               @include ('frontend.tour.partials._gallery')
+    @include ('frontend.tour.partials._gallery')
             </div>
         </li>
     </ul>
 </div>
-<div class="uk-card uk-card-default uk-card-body uk-margin-small-top  uk-hidden@l" id="mob-enquiry"> 
+<div class="uk-card uk-card-default uk-card-body uk-margin-small-top  uk-hidden@l" id="mob-enquiry">
     @include('frontend.tour.partials.modal._enquiry-mob')
 </div>
--- resources/views/frontend/tour/partials/_tabs.blade.php
+++ resources/views/frontend/tour/partials/_tabs.blade.php
@@ -4,13 +4,14 @@
 	<div class="uk-panel uk-text-center" uk-sticky="offset: 202" id="product-price-wrapper1">
 
 		<div class="uk-flex uk-flex-column sticky-price" id="product-price">
-			<div class="uk-card uk-card-default uk-card-body uk-margin-small-bottom text-color">		
-				<span class="top"> 
+			<div class="uk-card uk-card-default uk-card-body uk-margin-small-bottom text-color">
+				<span class="top">
 					{{ $tour->days }} Day(s)
 				</span>
 				<span class="middle uk-flex uk-flex-column ">
 					<span class="middle-1">From USD</span>
-					<span class="middle-2"> <sup>$</sup> {{ $tour->price }}</span>
+                    <span class="middle-2"> <sup>$</sup> {{ $tour->price }}</span>
+                    <span>per person</span>
 				</span>
 				@if($tour->departure->count() > 0)
 				<span class="bottom uk-padding-remove-bottom  uk-flex  uk-flex-column" uk-scrollspy-nav="closest: a; scroll: true; offset: 120">
@@ -20,10 +21,10 @@
 			</div>
 			<div class="uk-card uk-card-default uk-card-body trip-details">
 				<ul class="uk-list uk-list-divider text-color">
-					<li>				
+					<li>
 						<div class="uk-grid-small uk-child-width-expand uk-flex-nowrap uk-flex-middle uk-grid" uk-grid="">
 							<div class="uk-width-auto uk-first-column">
-								<i class="fas fa-info"></i>      
+								<i class="fas fa-info"></i>
 							</div>
 							<div  uk-scrollspy-nav="closest: a; scroll: true; offset: 120">
 								<a href="#enquiry" class="uk-link-text uk-link-reset" ><span>Quick Enquiry</span></a>
@@ -34,7 +35,7 @@
 					<li>
 						<div class="uk-grid-small uk-child-width-expand uk-flex-nowrap uk-flex-middle uk-grid" uk-grid="">
 							<div class="uk-width-auto uk-first-column">
-								<i class="fas fa-envelope"></i>   
+								<i class="fas fa-envelope"></i>
 							</div>
 							<div>
 								<a href="#modal-refer-friend" class="uk-link-text uk-link-reset" uk-toggle><span>Email a friend</span></a>
@@ -56,18 +57,18 @@
 						</div>
 					</li>
 					@endif
-				</ul>	
+				</ul>
 			</div>
 			{{-- sim card start --}}
 	{{-- 		<div class="uk-card uk-card-body uk-card-default sim-wrapper  uk-padding-remove-top">
 				<span>Ncell Sim Card</span>
-					
+
 			</div> --}}
 			{{-- sim card end --}}
 			{{-- Uno start --}}
 	{{-- 		<div class="uk-card uk-card-body uk-card-default uno-wrapper uk-padding-remove-top">
 				<span>uno offer</span>
-					
+
 			</div> --}}
 			{{-- uno end --}}
 		</div>
@@ -75,10 +76,10 @@
 </div>
 <div>
 	<div>
-		<div class="uk-card uk-card-default uk-visible@l">	
+		<div class="uk-card uk-card-default uk-visible@l">
 			<div class="uk-panel " id="tab-wrapper"  uk-sticky="offset: 0" >
 				<ul uk-scrollspy-nav="closest: li; scroll: true; offset: 140" class="uk-nav uk-nav-default uk-nav-center tm-nav uk-nav-parent-icon  uk-flex  uk-flex-row text-color tab-ul" uk-nav>
-					<li class="uk-active uk-width-1-5 tab-li"><a href="#overview">Overview</a></li> 
+					<li class="uk-active uk-width-1-5 tab-li"><a href="#overview">Overview</a></li>
 					<li class="uk-width-1-5  tab-li"><a href="#include">Inclusion</a></li>
 					@if($tour->itinerary->count() > 0)
 					<li class="uk-width-1-5  tab-li"><a href="#itinerary">Itinerary</a></li>
@@ -88,7 +89,7 @@
 					@endif
 					<li class="uk-width-1-5  tab-li"><a href="#gallery">Gallery</a></li>
 				</ul>
-			</div>	
+			</div>
 		</div>
 	</div>
 </div>

cb62fc720a8e54b0f64bd395ef83c769f097fb52
Bantawa Pawan <rahunn3@gmail.com>
2019-02-25 13:50:51 +0545

    partners

--- resources/views/frontend/pages/partners.blade.php
+++ resources/views/frontend/pages/partners.blade.php
@@ -11,23 +11,9 @@ Nepal isn't a destination, it is an experience, an experience beyond the ordinar
 @section('content')
 <!-- image start -->
 <section class="uk-container-expand about " uk-grid>
-        <div class="uk-background-contain uk-background-muted uk-height-medium uk-panel uk-flex uk-flex-center uk-flex-middle" style="background-image: url({{asset('img/dolls.webp')}});">
-            <div class="uk-overlay-primary uk-position-cover overlay"></div>
-            <h1 class="page-heading uk-margin-large-left">Partners</h1>
-            <p class="page-subheading uk-margin-large-left uk-margin-top uk-margin-bottom">
-                Nepal isn't a destination, it is an experience;
-                an experience beyond the ordinary.
-            </p>
-            <div class="breadcrumb ">
-                <ul class="uk-breadcrumb" id="breadcrumb">
-                    <li><a href="/" uk-icon="icon: home" ></a></li>
-                    <li class="uk-disabled"><a href="{{ url()->current() }}">Partners</a></li>
-                </ul>
-            </div>
-        </div>
-	{{-- <div class="uk-height-large  uk-width-*@s  uk-background-cover uk-light page-cover" data-src="https://source.unsplash.com/1200x800/?flower" uk-img>
+	<div class="uk-height-large  uk-width-*@s  uk-background-cover uk-light page-cover" data-src="{{asset('img/dolls.webp')}}" uk-img>
 		<div class="uk-overlay-primary uk-position-cover overlay"></div>
-		<h1 class="page-heading uk-margin-large-left">Who We Are</h1>
+		<h1 class="page-heading uk-margin-large-left">partners</h1>
 		<p class="page-subheading uk-margin-large-left uk-margin-top uk-margin-bottom">
 			Nepal isn't a destination, it is an experience;
 			an experience beyond the ordinary.
@@ -35,10 +21,10 @@ Nepal isn't a destination, it is an experience, an experience beyond the ordinar
 		<div class="breadcrumb ">
 			<ul class="uk-breadcrumb" id="breadcrumb">
 				<li><a href="/" uk-icon="icon: home" ></a></li>
-				<li class="uk-disabled"><a href="{{ url()->current() }}">About</a></li>
+				<li class="uk-disabled"><a href="{{ url()->current() }}">Partners</a></li>
 			</ul>
 		</div>
-	</div> --}}
+	</div>
 </section>
 
 <!-- image end -->
@@ -47,7 +33,7 @@ Nepal isn't a destination, it is an experience, an experience beyond the ordinar
         <div class="uk-container">
             <h3>Media Partners</h3>
             <div uk-grid>
-                @for($i=1;$i<=4;$i++)
+                @for($i=1;$i<=6;$i++)
                 <div class="uk-width-auto">
                         <img data-src="https://source.unsplash.com/300x150/?logo" alt="" uk-img>
                 </div>

0c8cf50c985f1ecc110943c3153decf86355393b
Bantawa Pawan <rahunn3@gmail.com>
2019-03-19 11:18:50 +0545

    csr text

--- resources/views/frontend/partials/home/_review.blade.php
+++ resources/views/frontend/partials/home/_review.blade.php
@@ -2,8 +2,13 @@
 <div class="uk-container uk-margin-medium-top uk-margin-mediumm-bottom">
     <div class="uk-grid-divider " uk-grid >
         <div class="uk-width-1-2@m">
-            <h2 class=" uk-text-center  uk-text-center uk-margin-mediumm-bottom"><span>Company CSR</span></h2>
-            <p >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+            <h2 class=" uk-text-center  uk-text-center uk-margin-mediumm-bottom"><span>Our Approach</span></h2>
+            <p> We are here to make a positive contribution to society.
+                We believe being socially responsible & giving back is our way of life.
+                It's Time Elevate local communities to education & their welfare.
+                Small effort Towards the community can make a big difference,
+                after all priceless smiles is all that counts.
+            </p>
         </div>
         <div class="uk-width-1-2@m">
             <img data-src="{{ asset('img/1.jpg') }}" width="576" height="486" alt="Crusade Himalay in Donation Program" uk-img>

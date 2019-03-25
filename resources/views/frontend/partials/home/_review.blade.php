48542ba14103c72ca55d47c04eaf5573ce8dc1af
Bantawa Pawan <rahunn3@gmail.com>
2019-03-25 18:52:04 +0545

    price

--- resources/views/frontend/partials/home/_review.blade.php
+++ resources/views/frontend/partials/home/_review.blade.php
@@ -3,6 +3,7 @@
     <div class="uk-grid-divider " uk-grid >
         <div class="uk-width-1-2@m">
             <h2 class=" uk-text-center strike uk-margin-medium-bottom"><span>Our Approach</span></h2>
+            <p class="uk-text-center uk-margin-remove-top home-heading-secondary uk-margin-large-bottom  " id="home-sub-title"><span>Corporate Social Responsibility</span></p>
             <p> We are here to make a positive contribution to society.
                 We believe being socially responsible & giving back is our way of life.
                 It's Time Elevate local communities to education & their welfare.

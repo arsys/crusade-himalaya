cd7d4a9a2d0ddc8bf91e1952e3e18ee6f678cdc6
Bantawa Pawan <rahunn3@gmail.com>
2019-03-25 18:55:59 +0545

    price

--- resources/views/frontend/partials/home/_review.blade.php
+++ resources/views/frontend/partials/home/_review.blade.php
@@ -2,8 +2,8 @@
 <div class="uk-container uk-margin-medium-top uk-margin-mediumm-bottom">
     <div class="uk-grid-divider " uk-grid >
         <div class="uk-width-1-2@m">
-            <h2 class=" uk-text-center strike uk-margin-medium-bottom"><span>Our Approach</span></h2>
-            <p class="uk-text-center uk-margin-remove-top home-heading-secondary uk-margin-large-bottom  " id="home-sub-title"><span>Corporate Social Responsibility</span></p>
+                <h2 class="uk-h2  uk-text-center uk-margin-remove-bottom home-title uk-margin-large-bottom"><span>Our Approach</span></h2>
+    <p class="uk-text-center uk-margin-remove-top home-heading-secondary uk-margin-large-bottom  " id="home-sub-title"><span>Corporate Social Responsibility</span></p>
             <p> We are here to make a positive contribution to society.
                 We believe being socially responsible & giving back is our way of life.
                 It's Time Elevate local communities to education & their welfare.

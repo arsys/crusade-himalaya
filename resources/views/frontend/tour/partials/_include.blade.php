44bd2bfad8a5753447084b4f50d2aad57cfa4170
Bantawa Pawan <rahunn3@gmail.com>
2019-03-26 16:14:40 +0545

    inc & ex

--- resources/views/frontend/tour/partials/_include.blade.php
+++ resources/views/frontend/tour/partials/_include.blade.php
@@ -1,5 +1,5 @@
 <div class="uk-grid-divider uk-child-width-expand@s uk-margin-medium-bottom text-color include-wrapper" uk-grid>
-    @if (!empty($tour->includes) && !empty($tour->excludes) && !empty($tour->budgetIncludes) && !empty($tour->budgetExcludes))
+    @if ($tour->includes->count() > 0 && $tour->excludes->count() > 0 && $tour->budgetIncludes->count() > 0 && $tour->budgetExcludes->count() > 0)
     <div>
             <ul class="uk-child-width-expand" uk-tab>
                 <li class="uk-active"><a href="#">Luxury Style</a></li>
@@ -30,7 +30,7 @@
                     <div>
                     <h4>Includes</h4>
                     <ul class="uk-list includes">
-                        @foreach($tour->includes as $include)
+                        @foreach($tour->budgetIncludes as $include)
                         <li><span class="uk-margin-small-right included" uk-icon="check"></span>{{ $include->name }}</li>
                         @endforeach
                     </ul>
@@ -39,7 +39,7 @@
                     <div>
                         <h4 class="">Excludes</h4>
                         <ul class="uk-list excludes">
-                            @foreach($tour->excludes as $exclude)
+                            @foreach($tour->budgetExcludes as $exclude)
                             <li><span class="uk-margin-small-right excluded" uk-icon="close"></span>{{ $exclude->name }}</li>
                             @endforeach
                         </ul>

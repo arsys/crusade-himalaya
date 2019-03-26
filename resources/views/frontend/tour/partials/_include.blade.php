cf0523038002177a11c176d8ba5c5c94c89062ae
Bantawa Pawan <rahunn3@gmail.com>
2019-03-26 16:01:21 +0545

    inc & ex

--- resources/views/frontend/tour/partials/_include.blade.php
+++ resources/views/frontend/tour/partials/_include.blade.php
@@ -1,12 +1,33 @@
 <div class="uk-grid-divider uk-child-width-expand@s uk-margin-medium-bottom text-color include-wrapper" uk-grid>
+    @if (!empty($tour->includes) && !empty($tour->excludes) && !empty($tour->budgetIncludes) && !empty($tour->budgetExcludes))
     <div>
-        <ul class="uk-child-width-expand" uk-tab>
-            <li class="uk-active"><a href="#">Luxury Includeds & Excludeds</a></li>
-            <li><a href="#">Budget Includeds & Excludeds</a></li>
-        </ul>
-        <ul class="uk-switcher">
-            <li>
-                <div>
+            <ul class="uk-child-width-expand" uk-tab>
+                <li class="uk-active"><a href="#">Luxury Style</a></li>
+                <li><a href="#">Budget Style</a></li>
+            </ul>
+            <ul class="uk-switcher">
+                <li>
+                    <div>
+                        <h4>Includes</h4>
+                        <ul class="uk-list includes">
+                            @foreach($tour->includes as $include)
+                            <li><span class="uk-margin-small-right included" uk-icon="check"></span>{{ $include->name }}</li>
+                            @endforeach
+                        </ul>
+
+                    </div>
+                    <div>
+                        <h4 class="">Excludes</h4>
+                        <ul class="uk-list excludes">
+                            @foreach($tour->excludes as $exclude)
+                            <li><span class="uk-margin-small-right excluded" uk-icon="close"></span>{{ $exclude->name }}</li>
+                            @endforeach
+                        </ul>
+
+                    </div>
+                </li>
+                <li>
+                    <div>
                     <h4>Includes</h4>
                     <ul class="uk-list includes">
                         @foreach($tour->includes as $include)
@@ -14,18 +35,36 @@
                         @endforeach
                     </ul>
 
-                </div>
-                <div>
-                    <h4 class="">Excludes</h4>
-                    <ul class="uk-list excludes">
-                        @foreach($tour->excludes as $exclude)
-                        <li><span class="uk-margin-small-right excluded" uk-icon="close"></span>{{ $exclude->name }}</li>
-                        @endforeach
-                    </ul>
+                    </div>
+                    <div>
+                        <h4 class="">Excludes</h4>
+                        <ul class="uk-list excludes">
+                            @foreach($tour->excludes as $exclude)
+                            <li><span class="uk-margin-small-right excluded" uk-icon="close"></span>{{ $exclude->name }}</li>
+                            @endforeach
+                        </ul>
 
-                </div>
-            </li>
-            <li></li>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    @else
+    <div>
+        <h4>Includes</h4>
+        <ul class="uk-list includes">
+            @foreach($tour->includes as $include)
+            <li><span class="uk-margin-small-right included" uk-icon="check"></span>{{ $include->name }}</li>
+            @endforeach
         </ul>
+
+        <h4>Excludes</h4>
+        <ul class="uk-list includes">
+            @foreach($tour->excludes as $exclude)
+            <li><span class="uk-margin-small-right included" uk-icon="check"></span>{{ $exclude->name }}</li>
+            @endforeach
+        </ul>
+
     </div>
+    @endif
+
 </div>

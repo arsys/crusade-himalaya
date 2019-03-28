f90c1006100a9a3ad1c60491e419521ae9dfec51
Kajol2608 <rauniyarkajol2608@gmail.com>
2019-03-28 14:24:13 +0545

    h1 product

--- resources/views/frontend/tour/partials/_banner.blade.php
+++ resources/views/frontend/tour/partials/_banner.blade.php
@@ -1,7 +1,7 @@
 <section class=" uk-container-expand">
 	<div class="uk-height-large uk-width-*@s uk-background-cover uk-light page-cover uk-first-column" data-src="{{ asset($tour->image->banner) }}" sizes="(max-width:65.25em) 20vw,(max-width:37.5em) 30vw, 300px" uk-img>
 		<div class="uk-overlay-primary uk-position-cover overlay"></div>
-		<h3 class="uk-text-center uk-margin-auto uk-margin-auto-vertical product-main-heading">{{ $tour->title }}</h3>
+		<h1 class="uk-text-center uk-margin-auto uk-margin-auto-vertical product-main-heading">{{ $tour->title }}</h1>
 		<div class="breadcrumb uk-visible@l ">
 			<ul class="uk-breadcrumb  " id="breadcrumb">
 				<li><a href="/" uk-icon="icon: home"></a></li>

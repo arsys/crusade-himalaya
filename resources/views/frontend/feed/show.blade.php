58250dc08f167d194a830b7cce488b82141b1878
Bantawa Pawan <rahunn3@gmail.com>
2019-04-06 19:08:17 +0545

    feed

--- resources/views/frontend/feed/show.blade.php
+++ resources/views/frontend/feed/show.blade.php
@@ -3,71 +3,71 @@
     '<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL
 ?>
 <feed xmlns="http://www.w3.org/2005/Atom">
-    <title>{{$tour->title}}</title>
-    <days>{{$tour->days}}</days>
-    <LuxuryPrice>USD {{$tour->price}}</LuxuryPrice>
-    <BudgetPrice>USD {{$tour->budgetPrice}}</BudgetPrice>
+    <title> <![CDATA[ {{$tour->title}} ]]> </title>
+    <days> <![CDATA[ {{$tour->days}} ]]> </days>
+    <LuxuryPrice> <![CDATA[ USD {{$tour->price}} ]]> </LuxuryPrice>
+    <BudgetPrice> <![CDATA[ USD {{$tour->budgetPrice}} ]]> </BudgetPrice>
     @if (!empty($tour->singleSupp))
-    <SinglePersonAdditional>USD {{$tour->singleSupp}}</SinglePersonAdditional>
+    <SinglePersonAdditional> <![CDATA[ USD {{$tour->singleSupp}} ]]> </SinglePersonAdditional>
     @endif
-    <MaxElevation>{{$tour->elevation}}</MaxElevation>
-    <DifficultyLevel>{{$tour->difficulty->name}}</DifficultyLevel>
-    <GroupSize>{{$tour->group->name}}</GroupSize>
-    <Category>{{$tour->category->name}}</Category>
+    <MaxElevation> <![CDATA[{{ $tour->elevation }}]]> </MaxElevation>
+    <DifficultyLevel> <![CDATA[{{ $tour->difficulty->name }}]]> </DifficultyLevel>
+    <GroupSize> <![CDATA[{{ $tour->group->size }}]]> </GroupSize>
+    <Category>  <![CDATA[{{ $tour->category->name }}]]> </Category>
     @if (!empty($tour->region))
-    <Region>{{$tour->region->name}}</Region>
+    <Region>  <![CDATA[{{ $tour->region->name }}]]> </Region>
     @endif
-	<Accommodation>{{$tour->accommodation->name}}</Accommodation>
-	<MealPlan>{{$tour->meal->name}}</MealPlan>
-	<overview>
-        {!!$tour->overview!!}
+	<Accommodation>  <![CDATA[{{ $tour->accommodation->name }}]]> </Accommodation>
+	<MealPlan>  <![CDATA[{{ $tour->meal->name }}]]> </MealPlan>
+	<overview type="html">
+        <![CDATA[ {!!$tour->overview!!} ]]>
     </overview>
     <itinerary>
             @foreach($tour->itinerary as $itinerary)
             <Day {{ $itinerary->day }}></Day {{ $itinerary->day }}><Plan {{ $itinerary->title }}></Plan {{ $itinerary->title }}>
-            <Detail>{!! $itinerary->plan !!}</Detail>
+            <Detail><![CDATA[ {!!$itinerary->plan!!} ]]></Detail>
             @endforeach
     </itinerary>
     @if ($tour->includes->count() > 0 && $tour->excludes->count() > 0 && $tour->budgetIncludes->count() > 0 && $tour->budgetExcludes->count() > 0)
     <LuxuryIncludes>
         @foreach($tour->includes as $include)
-        <LuxuryInclude {{$loop->iteration}}>{{$include->name}}</LuxuryInclude>
+        <LuxuryInclude {{$loop->iteration}} type="html">  <![CDATA[{{ $include->name }}]]> </LuxuryInclude>
         @endforeach
     </LuxuryIncludes>
     <LuxuryExcludes>
         @foreach($tour->excludes as $exclude)
-        <LuxuryExclude {{$loop->iteration}}>{{$include->name}}</LuxuryExclude>
+        <LuxuryExclude {{$loop->iteration}} type="html">  <![CDATA[{{ $exclude->name }}]]> </LuxuryExclude>
         @endforeach
     </LuxuryExcludes>
     <BudgetIncludes>
-            @foreach($tour->includes as $include)
-            <BudgetInclude {{$loop->iteration}}>{{$include->name}}</BudgetInclude>
+            @foreach($tour->budgetIncludes as $include)
+            <BudgetInclude {{$loop->iteration}} type="html">  <![CDATA[{{ $include->name }}]]> </BudgetInclude>
             @endforeach
         </BudgetIncludes>
         <BudgetExcludes>
-            @foreach($tour->excludes as $exclude)
-            <BudgetExclude {{$loop->iteration}}>{{$include->name}}</BudgetExclude>
+            @foreach($tour->budgetExcludes as $exclude)
+            <BudgetExclude {{$loop->iteration}} type="html"> <![CDATA[{{ $exclude->name }}]]> </BudgetExclude>
             @endforeach
         </BudgetExcludes>
     @else
     <Includes>
             @foreach($tour->includes as $include)
-            <Include {{$loop->iteration}}>{{$include->name}}</Include>
+            <Include {{$loop->iteration}} type="html"><![CDATA[{{ $include->name }}]]></Include>
             @endforeach
         </Includes>
         <Excludes>
             @foreach($tour->excludes as $exclude)
-            <Exclude {{$loop->iteration}}>{{$include->name}}</Exclude>
+            <Exclude {{$loop->iteration}} type="html"><![CDATA[{{ $exclude->name }}]]></Exclude>
             @endforeach
         </Excludes>
     @endif
     <DepartureDates>
         @foreach ($tour->departure as $departure)
-        <StartDate>{{ $departure->start }}</StartDate>
-        <EndDate>{{ $departure->end }}</EndDate>
-        <SpaceLeft>{{ $departure->slot }}<SpaaceLeft>
+        <StartDate><![CDATA[{{ $departure->start }}]]></StartDate>
+        <EndDate><![CDATA[{{ $departure->end }}]]></EndDate>
+        <SpaceLeft><![CDATA[{{ $departure->end }}]]><SpaaceLeft>
         @endforeach
     </DepartureDates>
-	<created_at>{{$tour->created_at}}</created_at>
-	<updated_at>{{$tour->updated_at}}</updated_at>
+	<created_at>{{$tour->created_at->toAtomString()}}</created_at>
+	<updated_at>{{$tour->updated_at->toAtomString()}}</updated_at>
 </feed>

ff6383b7f473b4b88482645980095853badd5b8f
Kajol2608 <rauniyarkajol2608@gmail.com>
2019-04-14 16:23:51 +0545

    twako

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -78,19 +78,20 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
 
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/5353692.js"></script>
+    <!--Start of Tawk.to Script-->
     <script type="text/javascript">
-        var _smartsupp = _smartsupp || {};
-        _smartsupp.key = 'fc758b4b810a5a7a7ece662bbbb6e1d691f50e23';
-        window.smartsupp||(function(d) {
-          var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-          s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-          c.type='text/javascript';c.charset='utf-8';c.async=true;
-          c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
-        })(document);
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5cb30bc5d6e05b735b427c2b/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
     </script>
-    @yield('styles')
-    @yield('conversion')
+    <!--End of Tawk.to Script-->
+    @yield('styles') @yield('conversion')
 </head>
 
 <body>
